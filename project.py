f3de9a89c7ceaed46ac112996aa154c5d6a5c956
filip-hellwig <filepix13@gmail.com>
2022-03-11 14:52:08 +0100

    base

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-
 class Library:
     def __init__(self, name, city, number, books = {}, readers = {}, numberOfBooks = 0, numberOfReaders = 0) -> None:
         self.name = name
@@ -13,6 +12,15 @@ class Library:
         print(f"{self.name} nr. {self.number} in {self.city}") 
         print(f"Number of books: {len(self.books)}, Number of readers: {len(self.readers)}")
 
+    def printAllBooks(self):
+        for book in self.books:
+            self.books[book].printInfo()
+    
+    def printAllReaders(self):
+        for reader in self.readers:
+            self.readers[reader].printInfo()
+
+
     def addBook(self):
         book = self.Book()
         book.title = input("Title: ")
@@ -23,12 +31,26 @@ class Library:
         book.number = self.numberOfBooks - 1
         self.books[book.number] = book
 
-    def addManyBooks(self):
-        f = open("books.txt")
+    def addBook2(self, list):
+        book = self.Book()
+        book.title = list[0]
+        book.author = list[1]
+        book.date = list[2]
+        book.category = list[3]
+        self.numberOfBooks += 1
+        book.number = self.numberOfBooks - 1
+        self.books[book.number] = book
+
+    def addManyBooks(self, file):
+        f = open(file)
 
         for line in f:
+            line = line.replace("\n", "")
             newBook = line.split(", ")
-            print(line)
+            self.addBook2(newBook)
+
+        f.close()
+            
 
     def addReader(self):
         reader = self.Reader()
@@ -39,6 +61,26 @@ class Library:
         self.numberOfReaders += 1
         reader.number = self.numberOfReaders - 1
         self.readers[reader.number] = reader
+
+    def addReader2(self, list):
+        reader = self.Reader()
+        reader.name = list[0]
+        reader.surname = list[1]
+        reader.age = list[2]
+
+        self.numberOfReaders += 1
+        reader.number = self.numberOfReaders - 1
+        self.readers[reader.number] = reader
+
+    def addManyReaders(self, file):
+        f = open(file)
+
+        for line in f:
+            line = line.replace("\n", "")
+            newReader = line.split(", ")
+            self.addReader2(newReader)
+
+        f.close()
 
     def searchBookTitle(self, title):
         for i in self.books:
@@ -60,6 +102,29 @@ class Library:
 
         self.readers[indexReader].borrow(indexBook)
 
+    def giveBackBook(self):
+        who = input("Surname: ")
+        indexReader = self.searchReaderSurname(who)
+
+        whichBook = input("Title: ")
+        indexBook = self.searchBookTitle(whichBook)
+        self.books[indexBook].giveBack()
+
+        self.readers[indexReader].giveBack(indexBook)
+
+    def bookInfo(self):
+        whichBook = input("Title: ")
+        indexBook = self.searchBookTitle(whichBook)
+        self.books[indexBook].printInfo()
+
+    def readerInfo(self):
+        who = input("Surname: ")
+        indexReader = self.searchReaderSurname(who)
+        toRead = self.readers[indexReader].printInfo()
+        print(f"Borrowed books:")
+        for books in toRead:
+            print(f"{self.books[books].title}") 
+        
 
 
 
@@ -94,18 +159,68 @@ class Library:
 
         def printInfo(self):
             print(f"{self.name} {self.surname}, age: {self.age}")
-            print(f"Borrowed books {self.books[0]}") 
-        
+            return self.books
+
         def borrow(self, number):
             self.books.append(number)
 
+        def giveBack(self, number):
+            index = self.books.index(number)
+            self.books.pop(index)
+
+
+def menuOption(option, branch):
+    if option == 0:
+        print("Exiting...")
+        quit()
+    elif option == 1:
+        branch.borrowBook()
+    elif option == 2:
+        branch.giveBackBook()
+    elif option == 3:
+        branch.addBook()
+    elif option == 4:
+        branch.addReader()
+    elif option == 5:
+        file = input("Enter file name: ")
+        branch.addManyBooks(file)
+    elif option == 6:
+        file = input("Enter file name: ")
+        branch.addManyReaders(file)
+    elif option == 7:
+        branch.printAllBooks()
+    elif option == 8:
+        branch.printAllReaders()
+    elif option == 9:
+        branch.bookInfo()
+    elif option == 10:
+        branch.readerInfo()
+    else:
+        print("Wrong option...")
+
+
+def menuInfo():
+    print("1. Borrow book")
+    print("2. Give back book")
+    print("3. Add book")
+    print("4. Add reader")
+    print("5. Add many books")
+    print("6. Add many readers")
+    print("7. Print all books")
+    print("8. Print all readers")
+    print("9. Book info")
+    print("10. Reader info")
+    print("0. Exit")
+
+print("\n\n")
 filia1 = Library('Filia', 'Wrocław', 1)
+filia1.addManyBooks("books.txt")
+filia1.addManyReaders("readers.txt")
 filia1.printInfo()
-filia1.addManyBooks()
-filia1.addBook()
-filia1.addReader()
 
-filia1.borrowBook()
+while 1:
+    print("\n")
+    menuInfo()
+    userInput = int(input("\nEnter menu option: "))
+    menuOption(userInput, filia1)
 
-filia1.books[0].printInfo()
-filia1.readers[0].printInfo()
